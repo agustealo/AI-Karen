9d65a427e2982f9ff679715bc4ec0a493b8b49ad
Agustealo Johnson <agustealo@gmail.com>
2026-08-25 09:59:15 -0400

    COG-CONVERGE-1: expose canonical cognitive state envelope

---
+++ src/ai_karen_engine/core/cognitive/__init__.py
@@ -0,0 +1,27 @@
+"""Canonical cognitive state snapshots shared between Core cognitive domains."""
+
+from .state import (
+    AdaptiveRecommendationSnapshot,
+    AdaptiveSnapshot,
+    BeliefSnapshot,
+    CognitiveState,
+    ContextSnapshot,
+    GoalSnapshot,
+    MetaSnapshot,
+    PolicySnapshot,
+    ReasoningSnapshot,
+    SalienceSnapshot,
+)
+
+__all__ = [
+    "AdaptiveRecommendationSnapshot",
+    "AdaptiveSnapshot",
+    "BeliefSnapshot",
+    "CognitiveState",
+    "ContextSnapshot",
+    "GoalSnapshot",
+    "MetaSnapshot",
+    "PolicySnapshot",
+    "ReasoningSnapshot",
+    "SalienceSnapshot",
+]
