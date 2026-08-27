f0cae449a68f3a66d2ce3210a2226b9132ad168d
Agustealo Johnson <agustealo@gmail.com>
2026-08-27 17:01:58 -0400

    feat(memory): add temporal evolution domain

---
+++ src/ai_karen_engine/core/memory/temporal/__init__.py
@@ -0,0 +1,17 @@
+"""Canonical temporal memory evolution contracts."""
+
+from .service import (
+    MemoryTemporalEvolutionService,
+    TemporalEvolutionDecision,
+    TemporalEvolutionKind,
+    TemporalInterval,
+    TemporalVersion,
+)
+
+__all__ = [
+    "MemoryTemporalEvolutionService",
+    "TemporalEvolutionDecision",
+    "TemporalEvolutionKind",
+    "TemporalInterval",
+    "TemporalVersion",
+]
